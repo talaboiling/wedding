aa493758f4bada7f2157a82925c5a2f4fa8e8f3b
talaboiling <tsakko@mail.ru>
2026-04-30 00:16:51 +0500

    adding cors headers to apis

--- api/export.js
+++ api/export.js
@@ -1,6 +1,16 @@
 import { kv } from '@vercel/kv';
 
 export default async function handler(request, response) {
+  // Enable CORS
+  response.setHeader('Access-Control-Allow-Credentials', true);
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  response.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+
+  if (request.method === 'OPTIONS') {
+    return response.status(200).end();
+  }
+
   // Only allow GET requests
   if (request.method !== 'GET') {
     return response.status(405).json({ error: 'Method not allowed' });
